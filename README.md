afaf11739c80d57b456768dc12f9f2aba98c9b52
ajay nishad <ajaynishad782@gmail.com>
2024-09-01 12:20:24 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # deploy-mern-app
+
+# APIs
+
+1. signup
+2. login
+3. products
+
+# Auth based 
+
+# technology
+
+1. node js
+2. express js
+3. mongoDB
+
+# Authentication
+
+JWT Token
---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./Routes/AuthRouter');
+const ProductRouter = require('./Routes/ProductRouter');
+require('dotenv').config();
+require('./Models/db');
+const PORT = process.env.PORT;
+app.use(bodyParser.json());
+app.use(cors())
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+app.listen(PORT, () => {
+    console.log(`Server in Running on ${PORT}`);
+})
